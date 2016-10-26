b7ffeb2dc573f53666b39431c2806f631ccdff9d
Rushabh22 <A0135914@u.nus.edu>
2016-10-26 10:06:24 +0800

    Added Application History and Application Update History Entity Classes.

---
+++ src/main/java/io/_29cu/usmserver/core/model/entities/BaseValueObject.java
@@ -0,0 +1,55 @@
+package io._29cu.usmserver.core.model.entities;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+
+@MappedSuperclass
+public class BaseValueObject {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	@NotNull
+	private Date creationDate = new Date();
+	@NotNull
+	private Date lastUpdateDate = new Date();
+	@NotNull
+	private String createBy = "USM";
+	@NotNull
+	private String lastUpdateBy = "USM";
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+	public String getCreateBy() {
+		return createBy;
+	}
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+	public String getLastUpdateBy() {
+		return lastUpdateBy;
+	}
+	public void setLastUpdateBy(String lastUpdateBy) {
+		this.lastUpdateBy = lastUpdateBy;
+	}
+}
