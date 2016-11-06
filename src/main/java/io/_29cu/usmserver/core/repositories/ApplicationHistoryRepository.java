b66d7a7f69489684cde85575a46ec6415447aeaf
jingdongcn <a0135901@u.nus.edu>
2016-11-06 17:04:21 +0800

    Add the repository for Application History

---
+++ src/main/java/io/_29cu/usmserver/core/repositories/ApplicationHistoryRepository.java
@@ -0,0 +1,7 @@
+package io._29cu.usmserver.core.repositories;
+
+/**
+ * Created by jdon on 6/11/2016.
+ */
+public interface ApplicationHistoryRepository {
+}
---
+++ src/main/java/io/_29cu/usmserver/core/service/ApplicationHistoryService.java
@@ -0,0 +1,7 @@
+package io._29cu.usmserver.core.service;
+
+/**
+ * Created by jdon on 6/11/2016.
+ */
+public interface ApplicationHistoryService {
+}
