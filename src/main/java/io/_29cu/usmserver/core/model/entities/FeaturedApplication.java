959235354f04790a8b0c17e0e37bed3b3d615807
Rushabh22 <A0135914@u.nus.edu>
2016-12-10 17:01:49 +0800

    Added identifier for Featured application #30

--- src/main/java/io/_29cu/usmserver/core/model/entities/FeaturedApplication.java
+++ src/main/java/io/_29cu/usmserver/core/model/entities/FeaturedApplication.java
@@ -1,8 +1,13 @@
 package io._29cu.usmserver.core.model.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class FeaturedApplication extends ApplicationBrowsingList{
+	@Id
+    @GeneratedValue
+    private Long id;
 
 }
--- src/main/java/io/_29cu/usmserver/core/model/entities/TrendingApplication.java
+++ src/main/java/io/_29cu/usmserver/core/model/entities/TrendingApplication.java
@@ -1,8 +1,5 @@
 package io._29cu.usmserver.core.model.entities;
 
-import javax.persistence.Entity;
-
-@Entity
 public class TrendingApplication extends ApplicationBrowsingList{
 
 }
