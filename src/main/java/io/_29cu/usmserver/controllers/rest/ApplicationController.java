b6edb01e4d86e0d6115827627f27b197ccb365fd
Li YuFeng <a0120538@u.nus.edu>
2016-12-10 13:17:12 +0800

    #48 Block Application

--- src/main/java/io/_29cu/usmserver/controllers/rest/ApplicationController.java
+++ src/main/java/io/_29cu/usmserver/controllers/rest/ApplicationController.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 - 29cu.io and the authors of alpha-umi open source project
-
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-
- *     http://www.apache.org/licenses/LICENSE-2.0
-
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,13 +16,15 @@
 
 package io._29cu.usmserver.controllers.rest;
 
-import io._29cu.usmserver.core.model.entities.Application;
-import io._29cu.usmserver.core.service.ApplicationService;
-import io._29cu.usmserver.core.service.utilities.ApplicationList;
 import io._29cu.usmserver.controllers.rest.resources.ApplicationListResource;
 import io._29cu.usmserver.controllers.rest.resources.ApplicationResource;
 import io._29cu.usmserver.controllers.rest.resources.assemblers.ApplicationListResourceAssembler;
 import io._29cu.usmserver.controllers.rest.resources.assemblers.ApplicationResourceAssembler;
+import io._29cu.usmserver.core.model.entities.Application;
+import io._29cu.usmserver.core.model.entities.User;
+import io._29cu.usmserver.core.service.ApplicationService;
+import io._29cu.usmserver.core.service.UserService;
+import io._29cu.usmserver.core.service.utilities.ApplicationList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,34 +34,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/api/1/store/application")
+@RequestMapping("/api")
 public class ApplicationController {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private ApplicationService applicationService;
 
-    @Autowired
-    private ApplicationService applicationService;
+	@RequestMapping(value = "/1/store/application/{appId}", method = RequestMethod.GET)
+	public ResponseEntity<ApplicationResource> getApplication(
+			@PathVariable String appId
+	) {
+		Application application = applicationService.findApplication(appId);
+		if (null == application)
+			return new ResponseEntity<ApplicationResource>(HttpStatus.NOT_FOUND);
 
-    @RequestMapping(value = "/{appId}", method = RequestMethod.GET)
-    public ResponseEntity<ApplicationResource> getApplication(
-            @PathVariable String appId
-    ) {
-        Application application = applicationService.findApplication(appId);
-        if (null == application)
-            return new ResponseEntity<ApplicationResource>(HttpStatus.NOT_FOUND);
+		ApplicationResource applicationResource = new ApplicationResourceAssembler().toResource(application);
+		return new ResponseEntity<ApplicationResource>(applicationResource, HttpStatus.OK);
+	}
 
-        ApplicationResource applicationResource = new ApplicationResourceAssembler().toResource(application);
-        return new ResponseEntity<ApplicationResource>(applicationResource, HttpStatus.OK);
-    }
+	@RequestMapping(value = "/1/store/application/developer/{developerId}", method = RequestMethod.GET)
+	public ResponseEntity<ApplicationListResource> getApplicationsByDeveloper(
+			@PathVariable String developerId
+	) {
+		ApplicationList applicationList = applicationService.findApplicationsByDeveloper(developerId);
+		if (null == applicationList)
+			return new ResponseEntity<ApplicationListResource>(HttpStatus.NOT_FOUND);
 
-    @RequestMapping(value = "/developer/{developerId}", method = RequestMethod.GET)
-    public ResponseEntity<ApplicationListResource> getApplicationsByDeveloper(
-            @PathVariable String developerId
-    ) {
-        ApplicationList applicationList = applicationService.findApplicationsByDeveloper(developerId);
-        if (null == applicationList)
-            return new ResponseEntity<ApplicationListResource>(HttpStatus.NOT_FOUND);
+		ApplicationListResource applicationListResource = new ApplicationListResourceAssembler().toResource(applicationList);
+		return new ResponseEntity<ApplicationListResource>(applicationListResource, HttpStatus.OK);
+	}
 
-        ApplicationListResource applicationListResource = new ApplicationListResourceAssembler().toResource(applicationList);
-        return new ResponseEntity<ApplicationListResource>(applicationListResource, HttpStatus.OK);
-    }
+	@RequestMapping(path = "/0/admin/applications/{appId}/block", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> UpdateDeveloperApplication(
+			@PathVariable String appId
+	) {
+		// Let's get the user from principal and validate the userId against it.
+		User user = userService.findAuthenticatedUser();
+		if (user == null)
+			return new ResponseEntity<Boolean>(HttpStatus.FORBIDDEN);
+		Application application = applicationService.findApplicationByDeveloperIdAndAppId(user.getId(), appId);
+		if (application == null)
+			return new ResponseEntity<Boolean>(HttpStatus.PRECONDITION_FAILED);
 
+		Boolean result = applicationService.blockApplication(application);
+		return new ResponseEntity<Boolean>(result, HttpStatus.OK);
+	}
 }
