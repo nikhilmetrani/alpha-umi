22655df64c5e1fde2009a214f304e22df3ad7245
Rushabh22 <A0135914@u.nus.edu>
2016-12-11 16:39:45 +0800

    Review replies #41

---
+++ src/main/java/io/_29cu/usmserver/core/model/entities/ReviewReply.java
@@ -0,0 +1,45 @@
+package io._29cu.usmserver.core.model.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class ReviewReply extends BaseValueObject {
+
+	@ManyToOne
+	private Review review;
+
+	@OneToOne
+	@NaturalId
+	private User developer;
+
+	private String description;
+
+	public Review getReview() {
+		return review;
+	}
+
+	public void setReview(Review review) {
+		this.review = review;
+	}
+
+	public User getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(User developer) {
+		this.developer = developer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
