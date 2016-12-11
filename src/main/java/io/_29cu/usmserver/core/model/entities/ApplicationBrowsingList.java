2737bfdc5b53a8b434300ff326f3ee8da5351039
Rushabh22 <A0135914@u.nus.edu>
2016-12-11 13:01:09 +0800

    Bug fix

--- src/main/java/io/_29cu/usmserver/core/model/entities/ApplicationBrowsingList.java
+++ src/main/java/io/_29cu/usmserver/core/model/entities/ApplicationBrowsingList.java
@@ -2,14 +2,27 @@ package io._29cu.usmserver.core.model.entities;
 
 import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 
 @MappedSuperclass
 public class ApplicationBrowsingList {
+	@Id
+	@GeneratedValue
+	private Long id;
 
 	@OneToMany
 	List<Application> applications;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public List<Application> getApplications() {
 		return applications;
