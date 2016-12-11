ca5431a37602a9a79bae5f8a67645a7619ca3edc
Rushabh22 <A0135914@u.nus.edu>
2016-12-11 16:23:19 +0800

    Review use case #41

---
+++ src/main/java/io/_29cu/usmserver/core/model/entities/Review.java
@@ -0,0 +1,74 @@
+package io._29cu.usmserver.core.model.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class Review extends BaseValueObject {
+	
+	@OneToOne
+	Application application;
+
+	@OneToOne
+	@NaturalId
+	private User consumer;
+
+	private String title;
+
+	private String description;
+	
+	private boolean featured;
+	
+	private Boolean like;	 
+
+	public boolean isFeatured() {
+		return featured;
+	}
+
+	public void setFeatured(boolean featured) {
+		this.featured = featured;
+	}
+
+	public Boolean getLike() {
+		return like;
+	}
+
+	public void setLike(Boolean like) {
+		this.like = like;
+	}
+
+	public Application getApplication() {
+		return application;
+	}
+
+	public void setApplication(Application application) {
+		this.application = application;
+	}
+
+	public User getConsumer() {
+		return consumer;
+	}
+
+	public void setConsumer(User consumer) {
+		this.consumer = consumer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
