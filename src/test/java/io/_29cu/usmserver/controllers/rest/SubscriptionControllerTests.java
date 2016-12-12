40c9181d8b4230444d77087eb73526ab88e724ba
jingdongcn <a0135901@u.nus.edu>
2016-12-12 23:15:18 +0800

    #31 unit test - testSubscribeApplicationIsForbidden

---
+++ src/test/java/io/_29cu/usmserver/controllers/rest/SubscriptionControllerTests.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2016 - 29cu.io and the authors of alpha-umi open source project
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package io._29cu.usmserver.controllers.rest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import io._29cu.usmserver.core.model.entities.*;
+import io._29cu.usmserver.core.model.enumerations.AppState;
+import io._29cu.usmserver.core.service.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class SubscriptionControllerTests {
+    @InjectMocks
+    private SubscriptionController subscriptionController;
+    @Mock
+    private SubscriptionService subscriptionService;
+    @Mock
+    private UserService userService;
+
+
+    // @Mock not used here since they are mocked in setup()
+    // Spring boot does not allow mocking with annotation.
+    SecurityContext securityContextMocked;
+    Authentication authenticationMocked;
+
+    private MockMvc mockMvc;
+
+    private User user;
+    private Application application;
+    private String uuid;
+
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(subscriptionController).build();
+
+        uuid = UUID.randomUUID().toString();
+
+        user = new User();
+        user.setId(1L);
+        user.setEmail("owner@test.com");
+        user.setUsername("Test Owner");
+        user.setEnabled(true);
+
+        application = new Application();
+        application.setId(uuid);
+        application.setCategory(new Category("Productivity"));
+        application.setName("Dreamweaver");
+        application.setState(AppState.Active);
+        application.setVersion("1.0");
+        application.setDeveloper(user);
+        application.setDescription("test description");
+
+        // Let's mock the security context
+        authenticationMocked = Mockito.mock(Authentication.class);
+        securityContextMocked = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContextMocked.getAuthentication()).thenReturn(authenticationMocked);
+        SecurityContextHolder.setContext(securityContextMocked);
+    }
+
+    @Test
+    public void  testSubscribeApplicationIsForbidden() throws Exception {
+        when(userService.findAuthenticatedUser()).thenReturn(null);
+
+        mockMvc.perform(post("/api/0/store/subscription/subscribe")
+                .content("{'application':{'name':'PhotoShop','downloadUrl':'https://test.com/photoshop', 'version':'1.1', 'category': { 'name': 'Lifestyle'}, 'state': 'Staging', 'description':'PhotoShop Description'}, 'user':{'username': 'Test Owner', 'email': 'owner@test.com', 'password': 'password'},'dateSubscribed':'2016/11/11', 'active':'True'}".replaceAll("'",  "\""))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+}
