e25977c0ec5a566d9a5176ffd172aae49b8c897c
Rushabh22 <A0135914@u.nus.edu>
2016-12-16 19:30:13 +0800

    Feature application repository #34 #30 #60

--- src/main/java/io/_29cu/usmserver/core/repositories/FeaturedApplicationRepository.java
+++ src/main/java/io/_29cu/usmserver/core/repositories/FeaturedApplicationRepository.java
@@ -1,14 +1,21 @@
 package io._29cu.usmserver.core.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
-import io._29cu.usmserver.core.model.entities.ApplicationBrowsingList;
+import io._29cu.usmserver.core.model.entities.Application;
 import io._29cu.usmserver.core.model.entities.FeaturedApplication;
 
-public interface ApplicationListRepository extends CrudRepository<ApplicationBrowsingList, String> {
+@Component
+public interface FeaturedApplicationRepository extends CrudRepository<FeaturedApplication, Long> {
 
-	@Query("select featuredApps from FeaturedApplication featuredApps")
-	FeaturedApplication findFeaturedApplications();
-	
+	@Query("select featuredApps.application from FeaturedApplication featuredApps where featuredApps.featureAppState like 'Active'")
+	public List<Application> findFeaturedApplications();
+
+	@Query("select featuredApps.application from FeaturedApplication featuredApps where featuredApps.featureAppState like 'Active' and featuredApps.application.id=:applicationId")
+	public Application findFeaturedApplicationByApplId(String applicationId);
+
 }
